d230b7091be86b84cb27d912438d317d3445cfcc
Christian Lizama <cfla86@gmail.com>
2026-05-06 14:25:28 -0400

    refactor: relocate generated Prisma client to src/generated/prisma directory

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,6 +1,6 @@
 
 import 'server-only'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma/client'
 import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 import * as mariadb from 'mariadb'
 
